88741dbdf506c9d3821eb0f94be5c9db5c50c36d
manmitha <manmitha@DESKTOP-6KVTDPA>
2024-03-22 15:39:16 +0530

    i am commiting test 4

---
+++ src/demo/Test4.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Test4 {
+	
+	public static void main(String[] args) {
+		
+		String s ="jana";
+		
+		for(int i=0;i<100;i++) {
+			
+			System.out.println(s);
+		}
+	
+}
+}
